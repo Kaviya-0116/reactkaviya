258e06893df28534d6d0e1c41cd780e5417395cf
kaviya-0116 <tkaviya0116@gmail.com>
2025-12-12 10:47:06 +0530

    first time

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React from "react";
+class App extends React.Component
+{
+  render(){
+    return(<>
+      <h1>welcome to components</h1>
+      <h1>welcome to components</h1>
+      <h1>welcome to components</h1>
+      <p>Cillum pariatur irure enim eu. Deserunt irure occaecat esse ut irure elit deserunt ipsum ea elit cupidatat irure. Sint esse veniam eu quis exercitation. Commodo velit eiusmod excepteur reprehenderit proident.
+
+Aliqua velit aliqua qui consectetur cupidatat dolore elit incididunt duis sint nulla exercitation do dolor. Do adipisicing nisi velit fugiat sunt consequat cupidatat laboris. Adipisicing amet non ex officia sit irure ex deserunt deserunt. Magna ea qui elit velit ipsum reprehenderit. Culpa eu ullamco reprehenderit deserunt ipsum laborum esse irure. Commodo consectetur quis elit sint mollit aliqua qui pariatur ullamco.
+
+Cupidatat velit excepteur tempor aliquip qui aliquip anim officia voluptate amet ex duis deserunt ad. Consectetur veniam dolor laborum ea officia magna sunt nulla cupidatat adipisicing aute dolor. Aute id enim deserunt magna laborum enim ea dolore. Reprehenderit duis excepteur ex mollit eiusmod culpa culpa. Consequat sit sit nisi cupidatat eiusmod voluptate ex nisi.
+
+Ut cupidatat labore amet non excepteur eu adipisicing pariatur. Tempor Lorem aute velit anim velit. Ullamco nisi ut nisi fugiat laboris dolor quis tempor eu sint consequat ex. In esse amet velit quis officia ut velit pariatur aliquip id cupidatat sunt elit exercitation. Excepteur nulla sunt Lorem culpa. Amet ea laborum aliquip aliqua laborum anim id commodo.
+
+Officia qui sint excepteur laboris amet non Lorem eu elit. Commodo eiusmod sunt nostrud cupidatat. Fugiat est id laborum ut. Do elit dolore quis nisi aliquip nulla occaecat consequat est Lorem. Excepteur ut eu enim occaecat reprehenderit elit esse veniam eiusmod do incididunt. Ut pariatur deserunt ut elit ex consectetur amet sunt est voluptate deserunt nulla.</p>
+      
+
+
+  
+   </> )
+  }
+}
+export default App
+/*
+function App()
+{
+  return(
+    // <div>
+    // <h1>Welcome to react js</h1>
+    // <h1>Welcome to react js</h1>
+    // <h1>Welcome to react js</h1>
+    // <h1>Welcome to react js</h1>
+    // </div>
+  )
+}
+export default App */
+/*
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
